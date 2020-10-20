fe08d02617df62f5514ed1f2c5e9bd70a7800f80
Phu Lam <lam@Phus-MacBook-Pro.local>
2020-10-19 21:51:20 -0700

    connect file successful

--- sorting/sorting/urls.py
+++ sorting/sorting/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from sortingApp.views import Home
+from django.urls import path, include
+from sortingApp.views import HomeView, BubbleView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Home.as_view(), name='home'),
+    path('', HomeView.as_view()),
+    path('bubble/', BubbleView.as_view()),
 ]
--- sorting/sortingApp/views.py
+++ sorting/sortingApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-class Home(TemplateView):
+class HomeView(TemplateView):
     template_name = 'sortingApp/home.html'
+
+class BubbleView(TemplateView):
+    template_name = 'sortingApp/bubble.html'
