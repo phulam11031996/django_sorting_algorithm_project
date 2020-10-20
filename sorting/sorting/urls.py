f56db23bea0d40cc12dd75336d143ced99023dfe
Phu Lam <lam@Phus-MacBook-Pro.local>
2020-10-19 17:53:44 -0700

    connecting the first file

--- sorting/sorting/urls.py
+++ sorting/sorting/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from sortingApp.views import Home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home.as_view(), name='home'),
 ]
